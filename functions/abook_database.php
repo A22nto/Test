3bb9ae5c3104241ae8920304a1fa3b4e4fa701c6
Antonio D'Ambrosio <antodamb96@gmail.com>
2017-12-20 19:21:54 +0100

    A

--- functions/abook_database.php
+++ functions/abook_database.php
@@ -1,7 +1,6 @@
 <?php
 
-
-
+(require_once SM_PATH . 'class/MyException.php');
 /**
  * abook_database.php
  *
@@ -110,7 +109,7 @@ class abook_database extends addressbook_backend {
             if (empty($param['dsn']) ||
                 empty($param['table']) ||
                 empty($param['owner'])) {
-                throw new Exception("Parameter cannot be null");
+                throw new NullParException("Parameter cannot be null");
             }
 
             $this->dsn   = $param['dsn'];
@@ -132,13 +131,21 @@ class abook_database extends addressbook_backend {
             $this->open(true);
         }
         else {
-            throw new Exception('Invalid argument to constructor');
+            throw new InvalidArgException('Invalid argument to constructor');
         }
         }
     
-        catch (Exception $e) {
+        catch (InvalidArgException $e) {
             echo "Exception ".": ".$e->getMessage();
         }
+        
+        catch (NullParException $e) {
+            echo "Exception ".": ".$e->getMessage();
+        }
+        
+    }
+        
+    
 
     /**
      * Open the database.
@@ -497,7 +504,7 @@ class abook_database extends addressbook_backend {
         }
         return true;
     }
-} /* End of class abook_database */
+ /* End of class abook_database */
 
 // vim: et ts=4
 }
